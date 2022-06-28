01737dea744ccaeb2ce0611c40964afa9b5f29f7
Astha <ashumaggu33@gmail.com>
2022-06-28 15:07:00 +0530

    test cases implemented

--- menu_builder_api/tests.py
+++ menu_builder_api/tests.py
@@ -1,3 +1,95 @@
+from django.urls import reverse
+from rest_framework import status
 from django.test import TestCase
 
-# Create your tests here.
+
+class SectionTest(TestCase):
+    def setUp(self):
+        self.data = {'name': 'lunch special', 'description': 'lunch description'}
+        self.data_update = {'name': 'lunch special', 'description': 'lunch description', "id": 1}
+
+    def test_can_create_section(self):
+        response = self.client.post(reverse('section'), self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_read_section(self):
+        response = self.client.get(reverse('section'))
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def test_can_update_section(self):
+    #     response = self.client.patch(reverse('section_id',  kwargs={'id': '1'}), self.data_update)
+    #     print(response.data)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #
+    # def test_can_delete_section(self):
+    #     response = self.client.delete(reverse('section_id',  kwargs={'id': '1'}), self.data_update)
+    #     print(response.data)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class ItemTest(TestCase):
+    def setUp(self):
+        self.data = {'name': 'soup lunch', 'description': 'soup lunch description', 'price': 23.0,
+                     'section_id': 1, 'modifiers': ''}
+        self.data_update = {'name': 'soup lunch', 'description': 'soup lunch description', "id": 1}
+
+    # def test_can_create_items(self):
+    #     response = self.client.post(reverse('items'), self.data)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_read_items(self):
+        response = self.client.get(reverse('items'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def test_can_update_items(self):
+    #     response = self.client.patch(reverse('item_id',  kwargs={'id': '1'}), self.data_update)
+    #     print(response.data)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #
+    # def test_can_delete_items(self):
+    #     response = self.client.delete(reverse('item_id',  kwargs={'id': '1'}), self.data_update)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class ModifierTest(TestCase):
+    def setUp(self):
+        self.data = {'modifiers_description': 'mike', 'items_id': 1}
+        self.data_update = {'modifiers_description': 'mike', 'items_id': 1, "id": 1}
+
+    def test_can_create_modifier(self):
+        response = self.client.post(reverse('modifier'), self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_read_modifier(self):
+        response = self.client.get(reverse('modifier'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def test_can_update_modifier(self):
+    #     response = self.client.patch(reverse('modifier_id',  kwargs={'id': '1'}), self.data_update)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #
+    # def test_can_delete_modifier(self):
+    #     response = self.client.delete(reverse('modifier_id',  kwargs={'id': '1'}), self.data_update)
+    #     print(response.data)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class ModifierItemsMappingTest(TestCase):
+    def setUp(self):
+        # self.data = {'name': 'mike', 'description': 'Mike'}
+        self.url_user_list = ('http://127.0.0.1:8000/api/items-modifiers/')
+
+    def test_can_create_modifier_items(self):
+        response = self.client.get((self.url_user_list))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class SectionCompleteTest(TestCase):
+    def setUp(self):
+        # self.data = {'name': 'mike', 'description': 'Mike'}
+        self.data_update = {'name': 'mike', 'description': 'Mike', "id": 1}
+
+    def test_can_get_section_data(self):
+        response = self.client.get(reverse('section_data'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- menu_builder_api/urls.py
+++ menu_builder_api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import (
     MenuViews,
     ItemsViews,
@@ -10,15 +10,16 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'items-modifiers', OrderViewSet, basename='items-modifiers')
-
+# app_name = 'menu_builder_api'
 urlpatterns = [
+    path('', include(router.urls)),
     path('section/', MenuViews.as_view(), name='section'),
-    path('section/<int:id>', MenuViews.as_view()),
-    path('items/', ItemsViews.as_view()),
-    path('items/<int:id>', ItemsViews.as_view()),
-    path('modifiers/', ModifiersViews.as_view()),
-    path('modifiers/<int:id>', ModifiersViews.as_view()),
-    path('section_data/', ModifierItemsMenuViewSet.as_view()),
+    path('section/<int:id>', MenuViews.as_view(), name='section_id'),
+    path('items/', ItemsViews.as_view(), name='items'),
+    path('items/<int:id>', ItemsViews.as_view(), name='item_id'),
+    path('modifiers/', ModifiersViews.as_view(), name='modifier'),
+    path('modifiers/<int:id>', ModifiersViews.as_view(), name='modifier_id'),
+    path('section_data/', ModifierItemsMenuViewSet.as_view(), name='section_data'),
     # path('items-modifiers/', ItemsModifiersViewSet.as_view()),
 
 ]
